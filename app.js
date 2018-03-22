91655b8f5e134188976ad3840c5e099e434a3638
Muqsith Irfan <mui@one.com>
2018-03-22 17:44:53 +0400

    Created dummy htmls

--- app.js
+++ app.js
@@ -11,7 +11,25 @@ app.get('/api', (req, res) => {
     res.send(`Hello World`);
 });
 
-app.use('/arif', express.static(path.resolve(__dirname, 'dr', 'sites', 'arif')));
+app.use('/html/:username/js', (req, res, next) => {
+    let jsFilePath = path.resolve(__dirname, 'dr',
+        'sites', req.params.username, 'js', ...req.originalUrl.split('/js/')[1].split('/'));
+    res.set({'Content-Type': 'application/javascript'});
+    res.sendFile(jsFilePath);
+});
+
+app.use('/html/:username/css', (req, res, next) => {
+    let cssFilePath = path.resolve(__dirname, 'dr',
+        'sites', req.params.username, 'css', ...req.originalUrl.split('/css/')[1].split('/'));
+    res.set({'Content-Type': 'text/css'});
+    res.sendFile(cssFilePath);
+});
+
+app.use('/html/:username', (req, res, next) => {
+    let htmlFilePath = path.resolve(__dirname, 'dr', 'sites', req.params.username, 'index.html');
+    res.set({'Content-Type': 'text/html'});
+    res.sendFile(htmlFilePath);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
